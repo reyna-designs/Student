12eb59d855034899b7f752f302f19ab39a30ec3a
Czarina Sotelo <sotelocz@sheridancollege.ca>
2022-05-20 15:08:26 -0400

    this is my first commit

---
+++ src/Student.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author sotel
+ */
+public class Student {
+    
+    private int studentId;
+    private String studentName;
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public Student(int studentId, String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public static void main(String[] args) {
+        
+        System.out.println("Enter a word >");
+        
+        Scanner scan = new Scanner(System.in);
+        String userWord = scan.next();
+        
+        char[] wordLetter = new char[userWord.length()]; // Use length method for String
+        
+        for (int i = 0; i < wordLetter.length; i++) { // wordLetter is a property of an array, how long the array is
+            wordLetter[i] = userWord.charAt(i);
+        }
+        
+        for (int i = 0; i < wordLetter.length; i++) { 
+            System.out.println(wordLetter[i]);
+        }
+        
+        System.out.println("Printing in reverse order...");
+        
+        for (int i = wordLetter.length -1; i >= 0; i--) { 
+            System.out.println(wordLetter[i]);
+        }
+    
+    }
+    
+}
---
+++ src/student/StudentList.java
@@ -0,0 +1,25 @@
+
+/**
+ *
+ * @author sotel
+ */
+public class StudentList {
+    
+    public static void main(String[] args) {
+        Student[] studentList = new Student[3];
+        Student s1 = new Student(1, "Rina");
+        Student s2 = new Student(2, "Bobbert");
+        Student s3 = new Student(3, "Yazz");
+        
+        studentList[0] = s1;
+        studentList[1] = s2;
+        studentList[2] = s3;
+        
+        for (int i = 0; i < studentList.length; i++) {
+        System.out.println(studentList[i].getStudentName());
+    }
+    
+    }
+        
+    
+}
